30ca1fb62740b7f6b06002e8aa170b7cd31083dd
Yao Chen <142653427+YaoChen-2021@users.noreply.github.com>
2025-05-22 15:19:59 +0800

    Update Module2-1-Lipid class match.py

--- Module2-1-Lipid class match.py
+++ Module2-1-Lipid class match.py
@@ -1,6 +1,7 @@
 #Conversion of Lipid species from MS-DIAL to LipidSearch
 import os
 import pandas as pd
+
 def process_files(input_folder, index_file, output_folder):
     index_df = pd.read_excel(index_file)
     class_key_column = 'ClassKey'
@@ -11,22 +12,23 @@ def process_files(input_folder, index_file, output_folder):
             file_path = os.path.join(input_folder, file_name)
             input_df = pd.read_excel(file_path)
             if class_key_column not in input_df.columns or subclass_key_column not in input_df.columns:
-                print(f"Warning: File {file_name} is missing a ClassKey or SubClassKey column.")
-                continue
-
+                print(f"Warning: The file {file_name} is missing the ClassKey or SubClassKey columns")
+                continue      
             if 'Adduct' in input_df.columns:
-                input_df = input_df[
-                    ~((input_df['ClassKey'].isin(['TG', 'DG'])) & (input_df['Adduct'].str.strip() == 'M+H'))]
+                before_filter = input_df.shape[0]
+                
+                # Adduct  filtration：the M+H for TG and DG；M+HCOO for all lipids 
+                condition_tg_dg_h = (input_df['ClassKey'].isin(['TG', 'DG'])) & (input_df['Adduct'].str.strip() == 'M+H')
+                condition_hcooh = (input_df['Adduct'].str.strip() == 'M+HCOO')
+                combined_condition = condition_tg_dg_h | condition_hcooh
+                input_df = input_df[~combined_condition]
+                after_filter = input_df.shape[0]
 
-            # Match MS-DIAL and LipidSearch
             input_df[ontology_column] = input_df.apply(
                 lambda row: get_ontology_value(row[class_key_column], row[subclass_key_column], index_df,
-                                               ontology_column),
-                axis=1)
+                                               ontology_column),axis=1)
             output_file_path = os.path.join(output_folder, file_name)
             input_df.to_excel(output_file_path, index=False)
-            print(f"Processing complete: {file_name}")
-
 def get_ontology_value(class_key, subclass_key, index_df, ontology_column):
     matched_rows = index_df[(index_df['ClassKey'] == class_key) & (index_df['SubClassKey'] == subclass_key)]
     if not matched_rows.empty:
@@ -35,6 +37,6 @@ def get_ontology_value(class_key, subclass_key, index_df, ontology_column):
         return None
 
 input_folder = 'Module2-input-lipidsearch'
-index_file = 'Module2-1-index of class.xlsx'  #Default Index file name
+index_file = 'Module2-1-index of class.xlsx'  #Default Index file for nomenclature conversion
 output_folder = 'Module2-output-1'
 process_files(input_folder, index_file, output_folder)
